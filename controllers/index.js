c83e31c8eb9b2d37dd24b63506d67534ccc698d3
Aika Sat <aikasat95@gmail.com>
2020-03-01 00:52:30 +0600

    -finished routes for tracks.js + extra task

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.use('/artists', require('./artists'));
 router.use('/albums', require('./albums'));
-
+router.use('/tracks', require('./tracks'));
 
 module.exports = router;
